1004273aa347f8986f7e334abc009231a69a38c5
Sara Walker <sarawall1187@gmail.com>
2018-12-17 20:11:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+def sort_array_asc(int)
+  int.sort
+end
 
+def sort_array_desc(int)
+  int.sort { |a,b| b <=> a }
+end
+
+def sort_array_char_count(array)
+  array.sort { |a,b| a.length - b.length } 
+end
+
+def swap_elements(array)
+ array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(int)
+  int.reverse 
+end
+
+def kesha_maker(array)
+  #swap_array = []
+   array.each_with_index do |letter, index| 
+    letter == "$"
+  # swap_array << letter
+  end 
+ # swap_array
+end
